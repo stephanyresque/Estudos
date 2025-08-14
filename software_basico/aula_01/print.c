c5ca5783a9df274d0eeb3afff6a3e05c553e6e30
Stephany Resque <resquestephany@gmail.com>
2025-08-14 09:36:05 -0300

    Iniciando os estudos em software básico

---
+++ software_basico/aula_01/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Laboratorio aula 01!");
+    return 0;
+}
---
+++ software_basico/aula_01/say.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+
+void sayHello() 
+{ 
+   printf("Eu amo a nat!\n");
+}
