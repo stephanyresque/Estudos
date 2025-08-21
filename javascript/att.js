6e18c828145d745ff8f6e2e752a5b46c08eb51d5
Stephany Resque <resquestephany@gmail.com>
2025-08-21 14:55:48 -0300

    Update att.js

--- javascript/att.js
+++ javascript/att.js
@@ -1,30 +1,30 @@
-// let price = 300;
-// console.log(price);
+let price = 300;
+console.log(price);
 
-// price += 50;
-// console.log(price);
+price += 50;
+console.log(price);
 
-// price -= 50;
-// console.log(price);
+price -= 50;
+console.log(price);
 
-// price /= 50;
-// console.log(price);
+price /= 50;
+console.log(price);
 
-// console.log(50 > 25);
-// console.log(50 < 25);
+console.log(50 > 25);
+console.log(50 < 25);
 
-// console.log(50 >= 25);
-// console.log(50 <= 25);
+console.log(50 >= 25);
+console.log(50 <= 25);
 
-// console.log(50 == 25);
-// console.log(50 != 25);
+console.log(50 == 25);
+console.log(50 != 25);
 
-// console.log("50" == 50);
-// console.log("50" === 50);
-// console.log("50" === "50");
+console.log("50" == 50);
+console.log("50" === 50);
+console.log("50" === "50");
 
-// console.log("50" != 50);
-// console.log("50" !== 50);
-// console.log("50" !== "50");
+console.log("50" != 50);
+console.log("50" !== 50);
+console.log("50" !== "50");
 
 console.log("Stephany" == "stephany");
