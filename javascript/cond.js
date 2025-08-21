9b9dceed33a7e142452184c2ae76567cb89bb283
Stephany Resque <resquestephany@gmail.com>
2025-08-21 15:15:03 -0300

    Create cond.js

---
+++ javascript/cond.js
@@ -0,0 +1,22 @@
+// const number = 25;
+
+// if (number % 2 == 0)
+// {
+//     console.log("número par!");
+// }else
+// {
+//     console.log("número ímpar!");
+// }
+
+const nt = 10;
+
+if (nt >= 8.5)
+{
+    console.log("Aprovado com mérito!");
+}else if ( (nt < 8.5) & (nt >= 6) )
+{
+    console.log("Aprovado!");
+}else
+{
+    console.log("Reprovado!");
+}
