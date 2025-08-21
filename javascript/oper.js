792b5ea619ffc0bd733cbd38ab11e0d97fcfcd9f
Stephany Resque <resquestephany@gmail.com>
2025-08-21 13:23:46 -0300

    Update oper.js

---
+++ javascript/oper.js
@@ -0,0 +1,11 @@
+const sum = 20 + 7;
+const sub = 14 - 4;
+const mul = 20 * 20;
+const div = 10 / 5;
+const rest = 10 % 2;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(rest);
