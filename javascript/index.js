190121c14c873674ddad6d3e6cddcf951917fd04
Stephany Resque <resquestephany@gmail.com>
2025-08-22 11:25:47 -0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,15 @@
-console.log("Hello Word");
+// console.log("Hello Word");
+
+function main()
+{
+    const person = {
+        name : "Stephany",
+        age : 21,
+        isResearcher : true,
+    }
+
+    const {name, age} = person;
+
+    console.log(name, age);
+}
+main();
